c9230a211c75c57157e9151b5e020b442d6b6473
Shabonkerz <Shabonkerz@gmail.com>
2016-03-24 13:27:34 -0500

    remove block usage of pocket-calculator

--- tests/integration/components/pocket-calculator-test.js
+++ tests/integration/components/pocket-calculator-test.js
@@ -12,13 +12,4 @@ test('it renders', function(assert) {
   this.render(hbs`{{pocket-calculator}}`);
 
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#pocket-calculator}}
-      template block text
-    {{/pocket-calculator}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
